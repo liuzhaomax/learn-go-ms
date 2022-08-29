4d894bbec19dd9c2cc9b813a18cd5cd3c73027eb
LIU Zhao <liuzhaomax@163.com>
2022-08-29 23:04:05 +1000

    account model

---
+++ account_srv/internal/db.go
@@ -0,0 +1,42 @@
+package internal
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+	"learn-go-ms/account_srv/model"
+	"log"
+	"os"
+	"time"
+)
+
+var DB *gorm.DB
+var err error
+
+func InitDB() {
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold:             time.Second,
+			LogLevel:                  logger.Info,
+			IgnoreRecordNotFoundError: true,
+			Colorful:                  true,
+		},
+	)
+	conn := fmt.Sprintf("%s:%s@(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", "root", "123456", "localhost", 3306, "happy_account_mic_training")
+	DB, err = gorm.Open(mysql.Open(conn), &gorm.Config{
+		Logger: newLogger,
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+	})
+	if err != nil {
+		panic("数据库连接失败：" + err.Error())
+	}
+	err = DB.AutoMigrate(&model.Account{})
+	if err != nil {
+		fmt.Println(err)
+	}
+}
---
+++ account_srv/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "learn-go-ms/account_srv/internal"
+
+func init() {
+	internal.InitDB()
+}
+
+func main() {
+
+}
